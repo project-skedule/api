4e52db31da37040f188ac7ec13b7ceb1f62ae2ea
Dartt0n <antonkudryavtsevdoem@gmail.com>
2021-11-26 22:35:37 +0300

    format

--- models/web/outgoing/preview.py
+++ models/web/outgoing/preview.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import List
 
+
 class Subclass(BaseModel):
     educational_level: int = Field(ge=1, le=12)
     identificator: str = Field(max_length=50)

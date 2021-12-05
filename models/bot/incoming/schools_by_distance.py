3b3d034d5b6f174a88dd760f058e252b4efaff79
Dartt0n <antonkudryavtsevdoem@gmail.com>
2021-12-05 17:41:04 +0300

    Decrease min school name length to 1 symbol

--- models/bot/incoming/schools_by_distance.py
+++ models/bot/incoming/schools_by_distance.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Field, validator
 
 
 class SchoolsByDistance(BaseModel):
-    name: str = Field(max_length=200, min_length=10)
+    name: str = Field(max_length=200, min_length=1)

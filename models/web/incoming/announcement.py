9f4c6befb9f5b5ad4872ce27408a56ef30483646
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2022-02-02 18:26:14 +0000

    Announcements #61

--- models/web/incoming/announcement.py
+++ models/web/incoming/announcement.py
@@ -1,19 +1,19 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional, Union
-from typing_extensions import Annotated
 
 
 class Subclass(BaseModel):
-    educational_level: Optional[Annotated[int, Field(ge=1, le=12)]]
-    identificator: Optional[Annotated[str, Field(max_length=50)]]
-    additional_identificator: Optional[Annotated[str, Field(max_length=50)]]
+    educational_level: Optional[int] = Field(ge=1, le=12)
+    identificator: Optional[str] = Field(max_length=50)
+    additional_identificator: Optional[str] = Field(max_length=50)
 
 
 class Teacher(BaseModel):
-    name: Optional[Annotated[str, Field(max_length=200, min_length=1)]]
+    name: str = Field(max_length=200, min_length=1)
 
 
 class Announcement(BaseModel):
-    text: str = Field(max_length=4000)
+    link: str = Field(max_length=250, min_length=20, regex="https://telegra.ph/.*")
     school_id: int = Field(ge=1, le=2147483647)
-    filters: Optional[List[Union[Subclass, Teacher]]]
+    filters: List[Union[Teacher, Subclass]]
+    resend_to_parents: bool

13787fd8509315a6ec777f0d44595172c092d71a
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2022-03-12 11:12:24 +0300

    Telegram id Int32 -> Int64 (#182)

--- models/bot/telegram/incoming/child.py
+++ models/bot/telegram/incoming/child.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class Child(BaseModel):
-    telegram_id: int = Field(ge=1, le=2147483647)
+    telegram_id: int = Field(ge=1, le=9223372036854775807)
     subclass_id: int = Field(ge=1, le=2147483647)
